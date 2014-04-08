5bb627fd57128a2d78629af0159aede7b0233305
Florian Rebaudo <florian.rebaudo@telecomnancy.eu>
2014-04-08 09:35:31 +0200

    Grammaire : No bug

---
+++ output/__Test__.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import org.antlr.runtime.*;
+import org.antlr.runtime.debug.DebugEventSocketProxy;
+
+
+public class __Test__ {
+
+    public static void main(String args[]) throws Exception {
+        fantastic4Lexer lex = new fantastic4Lexer(new ANTLRFileStream("/Users/baudo/Documents/TELECOM Nancy/Projet/Compilation/output/__Test___input.txt", "UTF8"));
+        CommonTokenStream tokens = new CommonTokenStream(lex);
+
+        fantastic4Parser g = new fantastic4Parser(tokens, 49100, null);
+        try {
+            g.prog();
+        } catch (RecognitionException e) {
+            e.printStackTrace();
+        }
+    }
+}
